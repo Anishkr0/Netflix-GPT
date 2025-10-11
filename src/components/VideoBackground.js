a003dbb82ca4d7ea58b797ca9684b9e3f2666a2a
anishkr01 <info.anishkrlpu@gmail.com>
2025-10-12 01:18:23 +0530

    Clean Code Practice Using Modular Code

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,26 +1,16 @@
-import { useEffect } from "react";
-import { API_options } from "../utils/constant";
+
+import {useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
+
+//fetch Trailer Video and update the store with Trailer Video Data
 
 const VideoBackground = ({ movieId }) => {
-  const getMovieVideos = async () => {
-    const data = await fetch('https://api.themoviedb.org/3/movie/1038392/videos?language=en-US',
-      API_options
-    );
-    const json = await data.json();
-    //console.log(json);
-
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
-  };
-
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
-
+  const trailerVideo = useSelector(store=>store.movies?.trailerVideo)
+      // here we call the custom hook to fetch and set the trailer video from useMovieTrailer hooks
+  useMovieTrailer(movieId);
   return (
     <div>
-      <iframe src={"https://www.youtube.com/embed/"+trailer.key} title="YouTube video player"
+      <iframe src={"https://www.youtube.com/embed/"+trailerVideo?.key} title="YouTube video player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
     </div>
   );
