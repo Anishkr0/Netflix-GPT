935758de985fc6ee3c5455962e6bfb4356509700
anishkr01 <info.anishkrlpu@gmail.com>
2025-10-17 19:18:17 +0530

    Cleaning the Unnecessary consoles

--- src/hooks/useNowPlayingMovie.js
+++ src/hooks/useNowPlayingMovie.js
@@ -1,24 +1,3 @@
-// import { useDispatch } from "react-redux";
-// import { useEffect } from "react";
-// import { API_options } from "../utils/constant";
-// import { addNowPlayingMovies } from "../utils/moviesSlice";
-// //WORKING FINE
-// const useNowPlayingMovie = (movieId) => {
-//     //fetching the data from the  TMDB API and Updadte the Store of redux
-//     // const Dispatch = useDispatch();
-//     const dispatch = useDispatch();
-//     const getNowPlayingMovies = async () => {
-//         const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1", API_options);
-
-//         const json = await data.json();
-//         console.log(json.results);
-//        dispatch(addNowPlayingMovies(json.results));
-//     };
-//     useEffect(() => {
-//         getNowPlayingMovies();
-//     },[]);
-// }
-// export default useNowPlayingMovie; 
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { API_options } from "../utils/constant";
