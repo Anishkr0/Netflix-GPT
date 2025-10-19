ee024666dc5e77b38a185102d4ede23eaddb0cd9
anishkr01 <info.anishkrlpu@gmail.com>
2025-10-19 23:41:43 +0530

    Make Seprate Components for SearchBar

--- src/components/GptSerchBar.js
+++ src/components/GptSerchBar.js
@@ -1,8 +1,11 @@
 const SerchBar=()=>{
     return(
-            <h2>
-                hello
-          </h2>
+        <div>
+            <form className="rounded-sm bg-black p-2 mt-8 mx-80 ">
+                <input className=" rounded-md p-4 m-4" type = "text" placeholder="What Would You like to Watch Today"/>
+                <button className="rounded-xl bg-red-800 p-3 mx-5 text-white" >Search</button>
+            </form>
+        </div>
     )
 }
 export default SerchBar;
