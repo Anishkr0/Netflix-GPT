2aa4d21b4b555433aa5afb381edf3e0cd62f5d10
anishkr01 <info.anishkrlpu@gmail.com>
2025-10-21 23:22:51 +0530

    feat(searchbar): add multi-language support for searching in native languages

--- src/components/GptSerchBar.js
+++ src/components/GptSerchBar.js
@@ -1,9 +1,11 @@
+import lang from "./LanguageConstant";
+
 const SerchBar=()=>{
     return(
         <div className="pt-[8%]">
             <form className="w-1/2 rounded-lg bg-black  mx-80 flex justify-center align-center grid grid-cols-12">
-                <input className=" col-span-10 rounded-md p-4 m-4 border border-red-300" type = "text" placeholder="What Would You like to Watch Today"/>
-                <button className="rounded-xl bg-red-800 p-3 mx-5 m-4 text-white col-span-2" >Search</button>
+                <input className=" col-span-10 rounded-md p-4 m-4 border border-red-300" type = "text" placeholder={lang.hindi.gptSearchPlaceholder}/>
+                <button className="rounded-xl bg-red-800 p-3 mx-5 m-4 text-white col-span-2" >{lang.hindi.search}</button>
             </form>
         </div>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,10 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { addUser, removeUser } from "../utils/userSlice";
-import { NETFLX_LOGO } from "../utils/constant";
+import { NETFLX_LOGO, SUPPORTED_LANGUAGE } from "../utils/constant";
 import { toogleGptSerchView } from "../utils/gptSlice";
+import lang from "./LanguageConstant";
+// import { SUPPORTED_LANGUAGE } from "../utils/constant";
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -39,10 +41,10 @@ const Header = () => {
     return () => unsubscribe();
   }, []);
 
-  const handleGptSearchClick = ()=>{
-     dispatch(toogleGptSerchView());
+  const handleGptSearchClick = () => {
+    dispatch(toogleGptSerchView());
     //Toggle GPT
-  }
+  };
   return (
     <div className="absolute  px-9 py-2 bg-gradient-to-b from-black z-40 flex justify-between w-full items-center">
       <img
@@ -52,9 +54,27 @@ const Header = () => {
       />
       {user && (
         <div className="flex">
-  <button className="px-2 py-2 m-2 text-white bg-black rounded-2xl" onClick={handleGptSearchClick}> GPT Search</button>
+          <select>
+            {SUPPORTED_LANGUAGE.map((lang) => (
+              <option key={lang.identifier} value={lang.identifier}>
+                {lang.name}
+              </option>
+            ))}
+
+            {/* <option value="hindi">Hindi</option>
+            <option value="spanish">Spanish</option> */}
+          </select>
+          <button
+            className="px-2 py-2 m-2 text-white bg-black rounded-2xl"
+            onClick={handleGptSearchClick}
+          >
+            GPT Search
+          </button>
           <img className="w-13 h-12  rounded-full" src={user?.photoURL} />
-          <button className= " text-white bg-black  px-2 py-2 m-2  rounded-2xl" onClick={handleSignOut}>
+          <button
+            className=" text-white bg-black  px-2 py-2 m-2  rounded-2xl"
+            onClick={handleSignOut}
+          >
             (SignOut)
           </button>
         </div>
